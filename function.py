2d9b6fda49d4bfd7742f6a9605213b379ce592f9
jmoradev <50599112+jmoradev@users.noreply.github.com>
2021-03-26 13:00:02 -0600

    Update function.py
    se actualizan los comentarios

--- function.py
+++ function.py
@@ -1,18 +1,17 @@
-"""
-Metodo para el truncado de digitos de algun numero flotante
- se requiere la importacion 
-    from math import trunc
-"""
+# ----------------------------------------------------------------
+# Metodo para el truncado de digitos de algun numero flotante
+#  se requiere la importacion
+#     from math import trunc
+# ----------------------------------------------------------------
 
 def truncate(number, digits) -> float:
     stepper = 10.0 ** digits
     return trunc(stepper * number) / stepper
 
 
-"""
-Metodo para la convercion de de bytes a unidades superiores
-"""
-
+# ----------------------------------------------------------------
+# Metodo para la convercion de de bytes a unidades superiores
+# ----------------------------------------------------------------
 def convert_bytes(quantity: float, convertion: str = 'B') -> float:
     divisor = 1024
     switcher = {
@@ -23,11 +22,10 @@ def convert_bytes(quantity: float, convertion: str = 'B') -> float:
     return switcher.get(convertion, lambda: "Invalid convertion")(quantity)
 
 
-"""
-Metodo para obtener el nombre base de un archivo
- se requiere la importacion
-    from os import path
-"""
-
+# -------------------------------------------------------
+# Metodo para obtener el nombre base de un archivo
+# se requiere la importacion
+#    from os import path
+# -------------------------------------------------------
 def file_name(file_path: str) -> str:
     return path.basename(file_path)
